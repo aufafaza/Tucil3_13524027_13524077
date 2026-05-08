6131377ac8d07b2eec0de81a2a98ae167bf346bf
aufafaza <aufa.rienaldifaza.a@gmail.com>
2026-05-08 14:01:09 +0700

    feat: a star to ui

--- src/main.cpp
+++ src/main.cpp
@@ -3,92 +3,18 @@
 #include "ui/snapshot.hpp"
 #include "ui/gameui.hpp"
 #include "ui/grideditor.hpp"
+#include "core/algorithm/aStar.hpp"
 #include <iostream>
-#include <queue>
-#include <unordered_map>
-
-struct StateHash2 {
-    size_t operator()(const core::State& s) const {
-        size_t h = std::hash<int>()(s.pos.row) ^ (std::hash<int>()(s.pos.col) << 1) ^ (std::hash<int>()(s.visitedCheckpoints) << 2);
-        return h;
-    }
-};
-
-static ui::SolutionLog buildBFSLog(core::Graph& graph) {
-    core::State init = graph.getInitialState();
-    std::queue<core::State> q;
-    std::unordered_map<core::State, core::State, StateHash2> parent;
-    std::unordered_map<core::State, char, StateHash2> parentMove;
-
-    q.push(init);
-    parent[init] = init;
-    parentMove[init] = 'S';
-
-    core::State goalState = init;
-    bool found = false;
-
-    while (!q.empty() && !found) {
-        core::State cur = q.front();
-        q.pop();
-
-        if (graph.isGoal(cur)) {
-            goalState = cur;
-            found = true;
-            break;
-        }
-
-        auto succs = graph.getSuccessors(cur);
-        for (const auto& s : succs) {
-            if (parent.find(s.state) == parent.end()) {
-                parent[s.state] = cur;
-                parentMove[s.state] = s.move;
-                q.push(s.state);
-            }
-        }
-    }
-
-    ui::SolutionLog log;
-    log.algorithmName = "BFS-dummy";
-    log.found = found;
-
-    if (found) {
-        std::vector<std::pair<core::State, char>> path;
-        core::State cur = goalState;
-        while (!(cur.pos.row == init.pos.row && cur.pos.col == init.pos.col && cur.visitedCheckpoints == init.visitedCheckpoints)) {
-            char m = parentMove[cur];
-            cur = parent[cur];
-            path.push_back({cur, m});
-        }
-        std::reverse(path.begin(), path.end());
-
-        log.snapshots.push_back({init.pos, init.visitedCheckpoints, 'S', 0});
-        int stepNum = 1;
-        for (auto& [st, mv] : path) {
-            log.snapshots.push_back({st.pos, st.visitedCheckpoints, mv, stepNum++});
-        }
-
-        log.totalSteps = static_cast<int>(log.snapshots.size());
-        log.iterations = static_cast<int>(parent.size());
-    } else {
-        core::State cur = init;
-        log.snapshots.push_back({cur.pos, cur.visitedCheckpoints, 'S', 0});
-        for (int i = 0; i < 9 && !graph.isGoal(cur); i++) {
-            auto succs = graph.getSuccessors(cur);
-            if (succs.empty()) break;
-            cur = succs[0].state;
-            log.snapshots.push_back({cur.pos, cur.visitedCheckpoints, succs[0].move, i + 1});
-        }
-        log.totalSteps = static_cast<int>(log.snapshots.size());
-        log.iterations = log.totalSteps;
-    }
-
-    log.executionTimeMs = 0;
-    return log;
-}
 
 static void runGame(const fileUtil::PuzzleData& pz) {
     core::Graph graph(pz);
-    ui::SolutionLog log = buildBFSLog(graph);
+    ui::SolutionLog log = algorithm::aStar(graph);
+
+    std::cout << "Algorithm: " << log.algorithmName << std::endl;
+    std::cout << "Found: " << (log.found ? "YES" : "NO") << std::endl;
+    std::cout << "Steps: " << log.totalSteps << std::endl;
+    std::cout << "Iterations: " << log.iterations << std::endl;
+    std::cout << "Time: " << log.executionTimeMs << " ms" << std::endl;
 
     ui::RenderConfig config;
     config.cellSize = 80;
